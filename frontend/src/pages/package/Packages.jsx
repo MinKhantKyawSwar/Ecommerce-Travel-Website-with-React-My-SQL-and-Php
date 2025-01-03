7c71a0c0e34784246ac14bd16ea6c0295478132a
MinKhantKyawSwar <mkks1986.mkks@gmail.com>
2025-01-03 04:39:34 -0800

    saved items noti toast added

--- frontend/src/pages/package/Packages.jsx
+++ frontend/src/pages/package/Packages.jsx
@@ -3,6 +3,9 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { FaRegBookmark, FaBookmark } from "react-icons/fa";
 import { UserContext } from "../../providers/UserContext";
+import "react-toastify/dist/ReactToastify.css";
+import { Bounce, Slide, ToastContainer, toast } from "react-toastify";
+
 
 const Packages = ({ destination_id, packages, setPackages }) => {
   const [error, setError] = useState(null);
@@ -63,6 +66,17 @@ const Packages = ({ destination_id, packages, setPackages }) => {
           setSavedPackages(updatedSavedPackages);
           localStorage.setItem(savedKey, updatedSavedPackages);
           updateSavedNoti(savedNoti - 1);
+          toast.success("Package successfully removed!", {
+            position: "top-center",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+          });
         } else {
           setError("Failed to remove item");
         }
@@ -92,6 +106,17 @@ const Packages = ({ destination_id, packages, setPackages }) => {
           setSavedPackages(updatedSavedPackages);
           localStorage.setItem(savedKey, updatedSavedPackages);
           updateSavedNoti(Number(savedNoti) + 1);
+          toast.success("Package successfully saved!", {
+            position: "top-center",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+          });
         } else if (response.data.status === 2) {
           alert("Already saved");
         } else {
@@ -118,68 +143,83 @@ const Packages = ({ destination_id, packages, setPackages }) => {
   };
 
   return (
-    <div className="p-4 rounded-lg bg-gray-50 dark:bg-gray-800 mb-10">
-      {!error && packages.length > 0 ? (
-        <>
-          <p className="text-sm text-gray-500 dark:text-gray-400 mb-4">
-            {packages.length} packages Available
-          </p>
-          <div className="flex gap-10">
-            {packages.map((pkg) => (
-              <div
-                key={pkg.package_id}
-                className="border rounded-lg p-4 shadow-md w-full md:w-1/3"
-              >
-                <button
-                  onClick={() =>
-                    toggleSavedItem(pkg.package_id, pkg.least_price)
-                  }
-                  className="mb-2 px-2 py-2 rounded float-right text-lg"
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Slide}
+      />
+      <div className="p-4 rounded-lg bg-gray-50 dark:bg-gray-800 mb-10">
+        {!error && packages.length > 0 ? (
+          <>
+            <p className="text-sm text-gray-500 dark:text-gray-400 mb-4">
+              {packages.length} packages Available
+            </p>
+            <div className="flex gap-10">
+              {packages.map((pkg) => (
+                <div
+                  key={pkg.package_id}
+                  className="border rounded-lg p-4 shadow-md w-full md:w-1/3"
                 >
-                  {savedPackages.includes(pkg.package_id) ? (
-                    <FaBookmark />
-                  ) : (
-                    <FaRegBookmark />
-                  )}
-                </button>
-                <p className="font-medium text-xl mb-2">{pkg.package_name}</p>
-                <p className="text-sm mb-4">
-                  Starting from <b>${pkg.least_price}</b>/per person
-                  <br />
-                  <b>Facilities:</b> {pkg.facilities}
-                  <br />
-                  <b>Meals:</b> {pkg.meals}
-                  <br />
-                  <b>Activities:</b> {pkg.activities.substring(0, 30)}...
-                  <br />
-                  <b>Duration:</b> {pkg.duration}
-                </p>
-                <div className="flex justify-between">
                   <button
-                    className="border-2 border-blue-500 text-blue-500 px-3 py-2 rounded hover:bg-blue-500 hover:text-white transition"
-                    onClick={() => handleDetails(pkg.package_id)}
+                    onClick={() =>
+                      toggleSavedItem(pkg.package_id, pkg.least_price)
+                    }
+                    className="mb-2 px-2 py-2 rounded float-right text-lg"
                   >
-                    Check More
+                    {savedPackages.includes(pkg.package_id) ? (
+                      <FaBookmark />
+                    ) : (
+                      <FaRegBookmark />
+                    )}
                   </button>
-                  <button
-                    className="border-2 border-green-500 text-green-500 px-3 py-2 rounded hover:bg-green-500 hover:text-white transition"
-                    onClick={() => handleBooking(pkg.package_id)}
-                  >
-                    Book Package
-                  </button>
+                  <p className="font-medium text-xl mb-2">{pkg.package_name}</p>
+                  <p className="text-sm mb-4">
+                    Starting from <b>${pkg.least_price}</b>/per person
+                    <br />
+                    <b>Facilities:</b> {pkg.facilities}
+                    <br />
+                    <b>Meals:</b> {pkg.meals}
+                    <br />
+                    <b>Activities:</b> {pkg.activities.substring(0, 30)}...
+                    <br />
+                    <b>Duration:</b> {pkg.duration}
+                  </p>
+                  <div className="flex justify-between">
+                    <button
+                      className="border-2 border-blue-500 text-blue-500 px-3 py-2 rounded hover:bg-blue-500 hover:text-white transition"
+                      onClick={() => handleDetails(pkg.package_id)}
+                    >
+                      Check More
+                    </button>
+                    <button
+                      className="border-2 border-green-500 text-green-500 px-3 py-2 rounded hover:bg-green-500 hover:text-white transition"
+                      onClick={() => handleBooking(pkg.package_id)}
+                    >
+                      Book Package
+                    </button>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        </>
-      ) : (
-        !error && (
-          <p className="text-red-500">
-            No packages available for this destination.
-          </p>
-        )
-      )}
-    </div>
+              ))}
+            </div>
+          </>
+        ) : (
+          !error && (
+            <p className="text-red-500">
+              No packages available for this destination.
+            </p>
+          )
+        )}
+      </div>
+    </>
   );
 };
 
--- frontend/src/pages/package/SavedPackages.jsx
+++ frontend/src/pages/package/SavedPackages.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { Bounce, Slide, ToastContainer, toast } from "react-toastify";
 
 const SavedPackages = () => {
   const [error, setError] = useState(null);
@@ -41,7 +43,19 @@ const SavedPackages = () => {
       );
 
       if (response.data.status === 1) {
-        navigate(0)
+        toast.success("Package successfully removed!", {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+        setTimeout(() => navigate(0), 1000);
+
         setSavedItem((prevItems) =>
           prevItems.filter((item) => item.saved_id !== savedItemId)
         );
@@ -63,65 +77,81 @@ const SavedPackages = () => {
   }, []);
 
   return (
-    <div className="container mx-auto p-4 mb-10 mt-10 min-h-96">
-      <div className="text-lg font-bold mb-4">Saved Packages</div>
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Slide}
+      />
+      <div className="container mx-auto p-4 mb-10 mt-10 min-h-96">
+        <div className="text-lg font-bold mb-4">Saved Packages</div>
 
-      {savedItem.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {savedItem.map((item, index) => (
-            <div
-              key={index}
-              className="border border-gray-300 rounded-lg shadow-md p-4 flex flex-col"
-            >
-              {/* Image Section */}
-              {item.destination_image && (
-                <img
-                  src={`http://localhost:3000/backend/${item.destination_image}`}
-                  alt={item.package_name}
-                  className="w-full h-32 object-cover rounded-lg mb-4"
-                />
-              )}
+        {savedItem.length > 0 ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {savedItem.map((item, index) => (
+              <div
+                key={index}
+                className="border border-gray-300 rounded-lg shadow-md p-4 flex flex-col"
+              >
+                {/* Image Section */}
+                {item.destination_image && (
+                  <img
+                    src={`http://localhost:3000/backend/${item.destination_image}`}
+                    alt={item.package_name}
+                    className="w-full h-32 object-cover rounded-lg mb-4"
+                  />
+                )}
 
-              {/* Content Section */}
-              <div className="flex-grow">
-                <h1 className="text-xl font-semibold">
-                  {item.destination_name}
-                </h1>
-                <h3 className="text-lg font-light text-gray-700">
-                  {item.package_name}
-                </h3>
-                <p className="mt-4 text-lg font-bold text-green-600">
-                  Price: ${item.price}
-                </p>
-              </div>
+                {/* Content Section */}
+                <div className="flex-grow">
+                  <h1 className="text-xl font-semibold">
+                    {item.destination_name}
+                  </h1>
+                  <h3 className="text-lg font-light text-gray-700">
+                    {item.package_name}
+                  </h3>
+                  <p className="mt-4 text-lg font-bold text-green-600">
+                    Price: ${item.price}
+                  </p>
+                </div>
 
-              {/* Actions Section */}
-              <div className="flex flex-col justify-between mt-4">
-                <p className="text-gray-400 mb-4">
-                  <span className="font-medium">Saved At:</span> {item.saved_at}
-                </p>
-                <div className="flex space-x-2 justify-between">
-                  <button
-                    className="bg-green-500 text-white rounded-lg px-8 py-2 hover:bg-green-600 transition duration-200"
-                    onClick={(_) => handleBooking(item.package, index)}
-                  >
-                    Order
-                  </button>
-                  <button
-                    className="bg-red-500 text-white rounded-lg px-4 py-2 hover:bg-red-600 transition duration-200"
-                    onClick={(_) => deleteSavedItem(item.saved_id)}
-                  >
-                    Remove
-                  </button>
+                {/* Actions Section */}
+                <div className="flex flex-col justify-between mt-4">
+                  <p className="text-gray-400 mb-4">
+                    <span className="font-medium">Saved At:</span>{" "}
+                    {item.saved_at}
+                  </p>
+                  <div className="flex space-x-2 justify-between">
+                    <button
+                      className="bg-green-500 text-white rounded-lg px-8 py-2 hover:bg-green-600 transition duration-200"
+                      onClick={(_) => handleBooking(item.package, index)}
+                    >
+                      Order
+                    </button>
+                    <button
+                      className="bg-red-500 text-white rounded-lg px-4 py-2 hover:bg-red-600 transition duration-200"
+                      onClick={(_) => deleteSavedItem(item.saved_id)}
+                    >
+                      Remove
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p className="text-center text-gray-500">No saved items.</p>
-      )}
-    </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-center text-gray-500">No saved items.</p>
+        )}
+      </div>
+    </>
   );
 };
 
