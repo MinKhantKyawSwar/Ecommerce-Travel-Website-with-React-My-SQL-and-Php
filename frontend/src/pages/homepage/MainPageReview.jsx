862f7bc801015eac13719402af8f2eab5db22451
MinKhantKyawSwar <mkks1986.mkks@gmail.com>
2025-01-20 06:47:12 -0800

    homepage update

--- frontend/src/pages/homepage/MainPageReview.jsx
+++ frontend/src/pages/homepage/MainPageReview.jsx
@@ -19,9 +19,7 @@ const MainPageReview = ({ homePageReview }) => {
         transition={Slide}
       />
       <div className="mt-6">
-        <div className="flex leading-tight justify-between px-10">
-
-        </div>
+        <div className="flex leading-tight justify-between px-10"></div>
         <div className="mt-6 flex justify-center gap-4 items-center space-x-4 px-10">
           {homePageReview && homePageReview.length > 0 ? (
             homePageReview.slice(0, 5).map((review, index) => (
@@ -38,37 +36,40 @@ const MainPageReview = ({ homePageReview }) => {
                   alt={`${review.city} Destination`}
                   className="w-full h-[120px] object-cover rounded-t-lg mb-4"
                 />
-                <div className="flex items-center justify-center">
+                <div className="flex items-center ">
                   <img
                     src={`http://localhost:3000/backend/${review.profile_image}`}
                     alt={`${review.city} Destination`}
-                    className={`${index === 2 ? "w-16 h-16  object-cover rounded-full mb-4" : 
-                                "w-10 h-10 object-cover rounded-full"
-                    }`}
+                    className={`${index === 2
+                        ? "w-16 h-16 object-cover rounded-full mr-4"
+                        : "w-8 h-8 object-cover rounded-full mr-2"
+                      }`}
                   />
                   <div>
                     <h3
                       className={`${index === 2
                           ? "text-xl md:text-2xl font-bold text-gray-800"
                           : "text-sm md:text-xs font-bold text-gray-800"
-                        } truncate`}
+                        }`}
                     >
                       {review.username}
                     </h3>
                     <div className="flex items-center">
-                      <span className={`${index === 2 ? "text-yellow-500 text-lg": "text-yellow-500 text-sm"}`}>
+                      <span className={`${index === 2 ? "text-yellow-500 text-lg" : "text-yellow-500 text-sm"}`}>
                         {"★".repeat(review.rating)}
                       </span>
-                      <span className={`${index === 2 ? "text-gray-500 ml-2": "text-gray-500 text-sm"}`}>
+                      <span className={`${index === 2 ? "text-gray-500 ml-2" : "text-gray-500 text-sm"}`}>
                         ({review.rating} / 5)
                       </span>
                     </div>
                   </div>
                 </div>
-                <p className="text-gray-700 text-sm break-words">
-                  {review.review_description.length > 70
-                    ? review.review_description.substring(0,70) + "..."
-                    : review.review_description}
+                {/* Review Description */}
+                <p
+                  className={`${index === 2 ? "text-gray-700 text-sm" : "text-gray-700 text-xs"
+                    } break-words`}
+                >
+                  {review.review_description}
                 </p>
               </div>
             ))
@@ -77,6 +78,7 @@ const MainPageReview = ({ homePageReview }) => {
           )}
         </div>
       </div>
+
     </>
   );
 };
